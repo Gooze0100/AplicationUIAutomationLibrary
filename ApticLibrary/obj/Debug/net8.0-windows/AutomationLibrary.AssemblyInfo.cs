83541d1b4024bf84837a86f9b88f3eb959b48a94
kalnigi <kalniusg@gmail.com>
2024-10-13 14:55:32 +0300

    Added new settings.

--- ApticLibrary/obj/Debug/net8.0-windows/AutomationLibrary.AssemblyInfo.cs
+++ ApticLibrary/obj/Debug/net8.0-windows/AutomationLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutomationLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a6b98fdac586d1ca01ba56a895485a7ea01d2a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutomationLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutomationLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ExcelLibrary/obj/Debug/net8.0/ExcelLibrary.AssemblyInfo.cs
+++ ExcelLibrary/obj/Debug/net8.0/ExcelLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExcelLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a6b98fdac586d1ca01ba56a895485a7ea01d2a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExcelLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExcelLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
