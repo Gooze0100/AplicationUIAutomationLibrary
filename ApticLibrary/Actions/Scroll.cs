ad0aedcd645d09a6c0e8ab839dd1fff267750e75
kalnigi <kalniusg@gmail.com>
2024-10-13 14:46:35 +0300

    Initial commit.

---
+++ ApticLibrary/Actions/Scroll.cs
@@ -0,0 +1,14 @@
+﻿using FlaUI.Core.AutomationElements;
+
+namespace AutomationLibrary.Actions;
+
+public class Scroll
+{
+    public void ScrollIntoView(AutomationElement line)
+    {
+        if (line != null)
+        {
+            line.Patterns.ScrollItem.Pattern.ScrollIntoView();
+        }
+    }
+}
