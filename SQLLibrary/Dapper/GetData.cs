53cccbe698e1e059f3e182dcd4ec9036dc4315a8
kalnigi <kalniusg@gmail.com>
2024-10-15 13:24:35 +0300

    Created SQL library with Dapper.

---
+++ SQLLibrary/Dapper/GetData.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+
+namespace SQLLibrary.Dapper;
+
+public class GetData
+{
+    // So plug in can be greated and used in library, so we can create certain hooks in library and use as compiled dll in our application or user interface dynamically.
+    // Your application/user interface should know about library, but library should not know about UI, direction should be important.
+    // Except user interface library like WPF or something like that can know about it.
+    // Nuget packages adds libraries to your project, but also you can have private nuget packages feed by added new package source from specific place
+    // but for versioning you need to add to nuget package of your project.
+    // circular dependency is not good that because of the build process if goes in sequence and libraray says that it is depending on console app and then console app says that it is depending on library
+    // either one cannot be built because of this conflict of dependencies
+
+
+
+}
---
+++ SQLLibrary/Dapper/Internal/SqlDataAccess/SqlDataAccess.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SQLLibrary.Dapper.Internal.SqlDataAccess;
+
+internal class SqlDataAccess
+{
+    public static List<T> GetData<T, U>(string sqlQuery, U parameters, string connectionString)
+        where T : class
+    {
+        using (IDbConnection connection = new SqlConnection(connectionString))
+        {
+            IEnumerable<T> data = connection.Query<T>(sqlQuery, parameters);
+
+            List<T> dataList = [.. data];
+
+            return dataList;
+        }
+    }
+
+    public static List<T> GetDataSP<T, U>(string storedProcedure, U parameters, string connectionString)
+        where T : class
+    {
+        using (IDbConnection connection = new SqlConnection(connectionString))
+        {
+            var data = connection.Query(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+
+            List<T> dataList = [.. data];
+
+            return dataList;
+        }
+    }
+}
